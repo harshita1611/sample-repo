6a535bea49525310895ec5164b70fc38e03b4ee7
harshita1611 <harshitabhamhani5@gmail.com>
2024-12-26 23:34:26 +0530

    exception handling with proper message and response code

--- src/main/java/com/students/studentProfile/exception/GlobalExceptionHandler.java
+++ src/main/java/com/students/studentProfile/exception/GlobalExceptionHandler.java
@@ -3,22 +3,28 @@ package com.students.studentProfile.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Handle Validation Errors
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex){
-        Map<String ,String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+    // Handle Generic Exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex){
-        return new ResponseEntity<>("An error occured"+ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
