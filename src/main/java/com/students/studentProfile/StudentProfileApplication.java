cf170df80035216f529f6970ee04164891563999
Harshita Bhambhani <bhambhaniharshita16@gmail.com>
2025-01-28 18:31:55 +0530

    resolved erros

--- src/main/java/com/students/studentProfile/StudentProfileApplication.java
+++ src/main/java/com/students/studentProfile/StudentProfileApplication.java
@@ -3,11 +3,12 @@ package com.students.studentProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration.class
+})
 public class StudentProfileApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentProfileApplication.class, args);
 	}
-
 }
+
