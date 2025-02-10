d9e68cf4c5feba0871317d3dff604e55b40b960d
Harshita Bhambhani <bhambhaniharshita16@gmail.com>
2025-02-10 17:00:44 +0530

    added logging filter now it will work before the request goes into controller

---
+++ src/main/java/com/students/studentProfile/config/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.students.studentProfile.config;
+
+import com.students.studentProfile.service.StudentService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter implements Filter {
+    private final Logger logger= LoggerFactory.getLogger(LoggingFilter.class);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Optional: Initialization logic
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("Incoming request: " + request.getRemoteAddr());
+        logger.info("logging incoming request: {}",request.getRemoteAddr());
+        chain.doFilter(request, response); // Continue request processing
+    }
+
+    @Override
+    public void destroy() {
+        // Optional: Cleanup logic
+    }
+}
--- src/main/java/com/students/studentProfile/config/SwaggerConfig.java
+++ src/main/java/com/students/studentProfile/config/SwaggerConfig.java
@@ -16,20 +16,20 @@ public class SwaggerConfig {
                         .version("1.0")
                         .description("API for managing student profiles"));
     }
-    @Bean
-    public OpenApiCustomizer removePageSchema() {
-        return openApi -> {
-            if (openApi.getComponents() != null && openApi.getComponents().getSchemas() != null) {
-                // Remove specific pageable-related schemas
-                openApi.getComponents().getSchemas().remove("Page");
-                openApi.getComponents().getSchemas().remove("PageReport");
-                openApi.getComponents().getSchemas().remove("PageableObject");
-                openApi.getComponents().getSchemas().remove("PagePatient");
-                openApi.getComponents().getSchemas().remove("PageAppointment");
-                openApi.getComponents().getSchemas().remove("PageDoctor");
-                openApi.getComponents().getSchemas().remove("PageDepartment");
-                openApi.getComponents().getSchemas().remove("SortObject");
-            }
-        };
-    }
+//    @Bean
+//    public OpenApiCustomizer removePageSchema() {
+//        return openApi -> {
+//            if (openApi.getComponents() != null && openApi.getComponents().getSchemas() != null) {
+//                // Remove specific pageable-related schemas
+//                openApi.getComponents().getSchemas().remove("Page");
+//                openApi.getComponents().getSchemas().remove("PageReport");
+//                openApi.getComponents().getSchemas().remove("PageableObject");
+//                openApi.getComponents().getSchemas().remove("PagePatient");
+//                openApi.getComponents().getSchemas().remove("PageAppointment");
+//                openApi.getComponents().getSchemas().remove("PageDoctor");
+//                openApi.getComponents().getSchemas().remove("PageDepartment");
+//                openApi.getComponents().getSchemas().remove("SortObject");
+//            }
+//        };
+//    }
 }
