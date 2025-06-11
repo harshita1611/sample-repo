1e345e16306a1d2224e3c45bc6afcaed65beda45
Harshita Bhambhani <harshita.b@iqm.com>
2025-06-12 04:03:36 +0530

    added sample file

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        // This is a placeholder for the main method.
+        // You can add test cases or functionality here as needed.
+        System.out.println("Test class is running.");
+    }
+}
